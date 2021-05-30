862c95acd166cf3fba46c51aad79c24aeb1c2240
Safran <safouankhelifa@gmail.com>
2021-05-30 17:43:34 +0200

    d

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pano_20210529_185428_1",
+      "name": "PANO_20210529_185428_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.3032086059820642,
+        "pitch": -0.040810521470135086,
+        "fov": 1.1313704520441656
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9463399863439754,
+          "pitch": -0.002354256899458562,
+          "rotation": 0,
+          "target": "1-pano_20210529_190417_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pano_20210529_190417_2",
+      "name": "PANO_20210529_190417_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.1313704520441656
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.6847457920039144,
+          "pitch": 0.11878911864512531,
+          "title": "Außenansicht Biologie",
+          "text": "Hier sehen Sie die Außenansicht unserer neu gestalteten Biologie"
+        }
+      ]
+    }
+  ],
+  "name": "GAG VR",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
